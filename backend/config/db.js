0df3739d3d14f87d10acfb9d9c67c8368ee0dd9f
Islahihyaa <islah.ihya@gmail.com>
2024-06-03 18:44:52 +0700

    Create Authentiocation login register

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://{username}:{password}@cluster0.cvvrvh3.mongodb.net/food-umkm-delivery').then(()=>console.log("Database Connected"))
+    await mongoose.connect('mongodb+srv://delivery-food:dbDeliveryFood@cluster0.cvvrvh3.mongodb.net/food-umkm-delivery').then(()=>console.log("Database Connected"))
 }
